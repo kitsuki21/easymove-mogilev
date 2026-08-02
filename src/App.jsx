2142ed9380483c396346633f8cdfb90223ae8b5d
kitsuki <RavenLebron@yandex.by>
2026-08-02 20:19:14 +0300

    fix: исправлен регистр в импортах компонентов

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Calculator from "./components/Calculator";
 import Contact from "./components/Contact";
